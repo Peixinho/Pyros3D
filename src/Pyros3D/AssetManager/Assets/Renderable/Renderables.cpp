2d2bd2f9d7c499214925702841a671beb7440740
Duarte Peixinho <duarte.peixinho@gmail.com>
2013-08-28 00:18:36 +0100

    File not Added in Last Commit - Renderables.cpp

---
+++ src/Pyros3D/AssetManager/Assets/Renderable/Renderables.cpp
@@ -0,0 +1,20 @@
+
+#include "Renderables.h"
+
+//============================================================================
+// Name        : Renderables.cpp
+// Author      : Duarte Peixinho
+// Version     :
+// Copyright   : ;)
+// Description : Renderables
+//============================================================================
+
+namespace p3d {
+  
+    namespace Renderables {
+      
+        uint32 IGeometry::_InternalID = 0;
+        
+    };
+    
+};
