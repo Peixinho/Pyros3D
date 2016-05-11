51c1a5eccebeaaf391fb50c42302752a8e3ccb46
Duarte Peixinho <duarte.peixinho@gmail.com>
2016-05-11 17:52:28 +0100

    File missing in last commit

---
+++ include/Pyros3D/Rendering/PostEffects/Effects/ResizeEffect.h
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : ResizeEffect.h
+// Author      : Duarte Peixinho
+// Version     :
+// Copyright   : ;)
+// Description : Resize Effect
+//============================================================================
+
+#include <Pyros3D/Rendering/PostEffects/Effects/IEffect.h>
+
+#ifndef RESIZEEFFECT_H
+#define	RESIZEEFFECT_H
+
+namespace p3d {
+
+    class PYROS3D_API ResizeEffect : public IEffect {
+        public:
+            ResizeEffect(const uint32 Tex1, const uint32 width, const uint32 height);
+            virtual ~ResizeEffect();
+    };
+
+};
+
+#endif	/* VIGNETTEEFFECT_H */
