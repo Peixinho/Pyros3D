a1216c6815650b02dce439fceab18be37140af12
Duarte Peixinho <duarte.peixinho@gmail.com>
2016-05-13 18:52:04 +0100

    Missing File

---
+++ include/Pyros3D/Rendering/PostEffects/Effects/VignetteEffect.h
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : VignetteEffect.h
+// Author      : Duarte Peixinho
+// Version     :
+// Copyright   : ;)
+// Description : Vignette Effect
+//============================================================================
+
+#include <Pyros3D/Rendering/PostEffects/Effects/IEffect.h>
+
+#ifndef VIGNETTEEFFECT_H
+#define	VIGNETTEEFFECT_H
+
+namespace p3d {
+
+    class PYROS3D_API VignetteEffect : public IEffect {
+        public:
+            VignetteEffect(const uint32 Tex1, const f32 radius = 0.5f, const f32 softness = 0.2f);
+            virtual ~VignetteEffect();
+
+			void SetRadius(const f32 radius);
+			void SetSoftness(const f32 softness);
+
+        private:
+
+			Uniform 
+					screenDimensions,
+					radiusUniform,
+					softnessUniform;
+
+    };
+
+};
+
+#endif	/* VIGNETTEEFFECT_H */
