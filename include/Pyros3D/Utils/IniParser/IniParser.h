58c5d70e40ad8241791cff7f93016acea20a12f5
Duarte Peixinho <duarte.peixinho@gmail.com>
2016-06-16 13:00:46 +0100

    Small fix on IniParser returns false when parsing the file is empty

--- include/Pyros3D/Utils/IniParser/IniParser.h
+++ include/Pyros3D/Utils/IniParser/IniParser.h
@@ -18,108 +18,110 @@ using namespace std;
 #define	INIPARSER_H
 
 namespace p3d {
-    
-    class PYROS3D_API IniParser {
-        
-        public:
-        
-            IniParser() {}
-            
-            bool ParseFile(const std::string &file)
-            {
-                string line;
-                ifstream myfile (file.c_str());
-                if (myfile.is_open())
-                {
 
-                    bool groupSet = false;
+	class PYROS3D_API IniParser {
 
-                    // Group Name
-                    string grpName;
-                    // Options
-                    std::map<string, string> options;
+	public:
 
-                    while ( getline (myfile,line) )
-                    {
-                        std::size_t foundBracket1 = line.find('[');
-                        std::size_t foundBracket2 = line.find(']');
-                        if (foundBracket1<line.size() && foundBracket2<line.size())
-                        {
-                            // Its a Group
+		IniParser() {}
 
-                            if (groupSet)
-                            {
-                                groupSet = false;
-                                INI[grpName] = options;
-                                options.clear();
-                            }
+		bool ParseFile(const std::string &file)
+		{
+			string line;
+			ifstream myfile(file.c_str());
+			if (myfile.is_open())
+			{
 
-                            grpName = line.substr(foundBracket1+1,foundBracket2-1);
-                            groupSet = true;
-                        } else {
-                            // Its a Value
-                            std::size_t foundEqual = line.find('=');
-                            if (foundEqual<line.size())
-                              options[line.substr(0,foundEqual)] = line.substr(foundEqual+1,line.size());
-                        }
-                    }
+				bool groupSet = false;
 
-                    INI[grpName] = options;
-                    options.clear();
+				// Group Name
+				string grpName;
+				// Options
+				std::map<string, string> options;
+				uint32 countLines = 0;
+				while (getline(myfile, line))
+				{
+					std::size_t foundBracket1 = line.find('[');
+					std::size_t foundBracket2 = line.find(']');
+					if (foundBracket1<line.size() && foundBracket2<line.size())
+					{
+						// Its a Group
 
-                    myfile.close();
-                    
-                    return true;
-                    
-                } else {
-                    
-                    return false;   
-                }
-            }
-        
-            string GetValue(const string &Group, const string &Option)
-            {
-		string result = INI[Group][Option];
-		if (result[result.size()-1]=='\r') result.erase(result.size()-1);
-                return result;
-            }
-            
-            void SetValue(const string &Group, const string &Option, const string &Value)
-            {
-                INI[Group][Option]=Value;
-            }
+						if (groupSet)
+						{
+							groupSet = false;
+							INI[grpName] = options;
+							options.clear();
+						}
 
-            bool SaveToFile(const string &file)
-            {
-                ofstream myfile (file.c_str());
-                if (myfile.is_open())
-                {
-                    for (map<string, map<string,string> >::iterator i=INI.begin();i!=INI.end();i++)
-                    {
-                        myfile << "[" << (*i).first << "]\n";
-                        for (map<string,string>::iterator k = (*i).second.begin();k!=(*i).second.end();k++)
-                        {
-                            myfile << (*k).first << "=" << (*k).second << "\n";
-                        }
-                    }
-                    myfile.close();
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+						grpName = line.substr(foundBracket1 + 1, foundBracket2 - 1);
+						groupSet = true;
+					}
+					else {
+						// Its a Value
+						std::size_t foundEqual = line.find('=');
+						if (foundEqual<line.size())
+							options[line.substr(0, foundEqual)] = line.substr(foundEqual + 1, line.size());
+					}
+					countLines++;
+				}
 
-			const map<string, map<string, string> > &GetIniParsedMap() const { return INI; }
-        
-        private:
-            
-            // Map to Keep Configurations
-            map<string, map<string,string> > INI;
-            
-    };
-    
-    
-    
+				INI[grpName] = options;
+				options.clear();
+
+				myfile.close();
+
+				return countLines>0;
+
+			}
+			else {
+
+				return false;
+			}
+		}
+
+		string GetValue(const string &Group, const string &Option)
+		{
+			string result = INI[Group][Option];
+			if (result[result.size() - 1] == '\r') result.erase(result.size() - 1);
+			return result;
+		}
+
+		void SetValue(const string &Group, const string &Option, const string &Value)
+		{
+			INI[Group][Option] = Value;
+		}
+
+		bool SaveToFile(const string &file)
+		{
+			ofstream myfile(file.c_str());
+			if (myfile.is_open())
+			{
+				for (map<string, map<string, string> >::iterator i = INI.begin(); i != INI.end(); i++)
+				{
+					myfile << "[" << (*i).first << "]\n";
+					for (map<string, string>::iterator k = (*i).second.begin(); k != (*i).second.end(); k++)
+					{
+						myfile << (*k).first << "=" << (*k).second << "\n";
+					}
+				}
+				myfile.close();
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		const map<string, map<string, string> > &GetIniParsedMap() const { return INI; }
+
+	private:
+
+		// Map to Keep Configurations
+		map<string, map<string, string> > INI;
+
+	};
+
 };
 
 #endif /* INIPARSER_H */
