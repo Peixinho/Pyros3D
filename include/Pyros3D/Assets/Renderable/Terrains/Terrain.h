cbadd09ab279e043dfab9ce8587bf1ef3ff4b3ee
Duarte Peixinho <duarte.peixinho@gmail.com>
2017-04-11 20:08:06 +0100

    Another missing file

---
+++ include/Pyros3D/Assets/Renderable/Terrains/Terrain.h
@@ -0,0 +1,49 @@
+//============================================================================
+// Name        : Terrain
+// Author      : Duarte Peixinho
+// Version     :
+// Copyright   : ;)
+// Description : Terrain
+//============================================================================
+
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include <Pyros3D/Assets/Renderable/Primitives/Primitive.h>
+#include <Pyros3D/Rendering/Components/Rendering/RenderingComponent.h>
+#include <Pyros3D/Other/Export.h>
+
+namespace p3d {
+
+	class PYROS3D_API Terrain : public Primitive {
+
+		friend class TerrainRenderingComponent;
+
+		public:
+
+			Terrain(Texture* heightmap, const f32 dimensions, const uint32 segments, const f32 height = 1, bool smooth = false, bool repeatUV = false);
+		
+		protected:
+
+			int imgWidth, imgHeight, seg;
+			f32 unit;
+	};
+
+	class PYROS3D_API TerrainRenderingComponent : public RenderingComponent
+	{
+
+	protected:
+
+		f32 segunits;
+
+	public:
+
+		TerrainRenderingComponent(Terrain* renderable, IMaterial* Material = NULL);
+		bool GetHeightFromLocalCoords(Vec3 &coords);
+		bool GetHeightFromGlobalCoords(Vec3 &coords);
+
+	};
+
+};
+
+#endif /* TERRAIN_H */
