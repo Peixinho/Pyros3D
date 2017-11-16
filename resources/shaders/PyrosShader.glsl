d37dcce6828dddf7075fc0b174d9f4180f85d805
Duarte Peixinho <duarte.peixinho@chbaserver.hbalgarvio.min-saude.pt>
2017-11-16 11:03:51 +0000

    added transpose to glsl for GLES2 and Emscripten

--- resources/shaders/PyrosShader.glsl
+++ resources/shaders/PyrosShader.glsl
@@ -15,24 +15,50 @@ float DecodeFloatRGBA( vec4 rgba ) {
 }
 
 #if defined(GLES2) || defined(GLLEGACY)
-    #if defined(GLES2) 
-        highp 
+    
+    #if defined(EMSCRIPTEN) 
+        #define __highpMat3 highp mat3
+        #define __highpMat4 highp mat4
+        #define __highpVec3 highp vec3
+        #define __highpVec4 highp vec4
+    #else
+        #define __highpMat3 mat3
+        #define __highpMat4 mat4
+        #define __highpVec3 vec3
+        #define __highpVec4 vec4
     #endif
-            mat4 transpose(in highp mat4 inMatrix) {
-            highp vec4 i0 = inMatrix[0];
-            highp vec4 i1 = inMatrix[1];
-            highp vec4 i2 = inMatrix[2];
-            highp vec4 i3 = inMatrix[3];
 
-            highp mat4 outMatrix = mat4(
-                         vec4(i0.x, i1.x, i2.x, i3.x),
-                         vec4(i0.y, i1.y, i2.y, i3.y),
-                         vec4(i0.z, i1.z, i2.z, i3.z),
-                         vec4(i0.w, i1.w, i2.w, i3.w)
-                         );
+    __highpMat3 transpose3(in __highpMat3 inMatrix) {
+        __highpVec3 i0 = inMatrix[0];
+        __highpVec3 i1 = inMatrix[1];
+        __highpVec3 i2 = inMatrix[2];
 
-            return outMatrix;
-        }
+        __highpMat3 outMatrix = mat3(
+                     vec3(i0.x, i1.x, i2.x),
+                     vec3(i0.y, i1.y, i2.y),
+                     vec3(i0.z, i1.z, i2.z)
+                     );
+
+        return outMatrix;
+    }
+    __highpMat4 transpose4(in __highpMat4 inMatrix) {
+        __highpVec4 i0 = inMatrix[0];
+        __highpVec4 i1 = inMatrix[1];
+        __highpVec4 i2 = inMatrix[2];
+        __highpVec4 i3 = inMatrix[3];
+
+        __highpMat4 outMatrix = mat4(
+                     vec4(i0.x, i1.x, i2.x, i3.x),
+                     vec4(i0.y, i1.y, i2.y, i3.y),
+                     vec4(i0.z, i1.z, i2.z, i3.z),
+                     vec4(i0.w, i1.w, i2.w, i3.w)
+                     );
+
+        return outMatrix;
+    }
+#else
+    mat3 transpose3(in mat3 m) { return transpose(m); }
+    mat4 transpose4(in mat4 m) { return transpose(m); }
 #endif
 
 #ifdef VERTEX
@@ -529,7 +555,7 @@ float DecodeFloatRGBA( vec4 rgba ) {
         #if defined(TEXTRENDERING) || defined(BUMPMAPPING) || defined(PARALLAXMAPPING) || defined(ENVMAP) || defined(REFRACTION) || defined(DIFFUSE) || defined(CELLSHADING)
             vec3 Normal;
             #if defined(BUMPMAPPING) || defined(PARALLAXMAPPING)
-                Normal = normalize(transpose(vTangentMatrix) * (texture2D(uNormalmap, Texcoord).rgb * 2.0 - 1.0));
+                Normal = normalize(transpose3(vTangentMatrix) * (texture2D(uNormalmap, Texcoord).rgb * 2.0 - 1.0));
                 #if defined(DEFERRED_GBUFFER)
                     gbuffer_normals.xyz = vViewMatrix * vec4(Normal,0);
                 #endif
