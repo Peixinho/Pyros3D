f944d305cb93f90ef8c2843e122cec9299b23b63
Duarte Peixinho <duarte.peixinho@gmail.com>
2021-02-15 12:29:42 +0000

    Added MotionBlur Example to BaseExample

--- examples/MotionBlurExample/MotionBlurExample.cpp
+++ examples/MotionBlurExample/MotionBlurExample.cpp
@@ -10,12 +10,12 @@
 
 using namespace p3d;
 
-MotionBlurExample::MotionBlurExample() : ClassName(1024, 768, "Pyros3D - Rotating Cube", WindowType::Close | WindowType::Resize) {}
+MotionBlurExample::MotionBlurExample() : BaseExample(1024, 768, "Pyros3D - Rotating Cube", WindowType::Close | WindowType::Resize) {}
 
 void MotionBlurExample::OnResize(const uint32 width, const uint32 height)
 {
 	// Execute Parent Resize Function
-	ClassName::OnResize(width, height);
+	BaseExample::OnResize(width, height);
 
 	// Resize
 	Renderer->Resize(width, height);
@@ -29,9 +29,7 @@ void MotionBlurExample::OnResize(const uint32 width, const uint32 height)
 void MotionBlurExample::Init()
 {
 	// Initialization
-
-	// Initialize Scene
-	Scene = new SceneGraph();
+    BaseExample::Init();
 
 	// Initialize Renderer
 	Renderer = new ForwardRenderer(Width, Height);
@@ -41,51 +39,27 @@ void MotionBlurExample::Init()
 	// Projection
 	projection.Perspective(70.f, (f32)Width / (f32)Height, 1.f, 1000.f);
 
-	// Create Camera
-	Camera = new GameObject();
-	Camera->SetPosition(Vec3(0, 10, 400));
+	FPSCamera->SetPosition(Vec3(0, 10, 400));
 
 	// Material
 	material = new GenericShaderMaterial(ShaderUsage::Texture);
 	texture = new Texture();
-	texture->LoadTexture(STR(EXAMPLES_PATH)"/RotatingTexturedCube/assets/Texture.png", TextureType::Texture);
+	texture->LoadTexture(STR(EXAMPLES_PATH)"/assets/Texture.png", TextureType::Texture);
 	material->SetColorMap(texture);
 
 	// Create Game Object
 	CubeObject = new GameObject();
-	cubeMesh = new Model(STR(EXAMPLES_PATH)"/RacingGame/assets/delorean/delorean.p3dm");
+	cubeMesh = new Model(STR(EXAMPLES_PATH)"/assets/teapotLOD1.p3dm");
 	//cubeMesh = new Cube(15, 15, 15);
 	rCube = new RenderingComponent(cubeMesh);
 	CubeObject->Add(rCube);
-	CubeObject->SetScale(Vec3(30,30,30));
 
-	// Add Camera to Scene
-	Scene->Add(Camera);
 	// Add GameObject to Scene
 	Scene->Add(CubeObject);
 
 	EffectManager = new PostEffectsManager(Width, Height);
 	MotionBlur = new MotionBlurEffect(RTT::Color, VRenderer->GetTexture(),Width,Height);
 	EffectManager->AddEffect(MotionBlur);
-
-	SetMousePosition((uint32)(Width *.5f), (uint32)(Height *.5f));
-	mouseCenter = Vec2((f32)Width *.5f, (f32)Height *.5f);
-	mouseLastPosition = mouseCenter;
-	counterX = counterY = 0.f;
-
-	// Input
-	InputManager::AddEvent(Event::Type::OnPress, Event::Input::Keyboard::W, this, &MotionBlurExample::MoveFrontPress);
-	InputManager::AddEvent(Event::Type::OnPress, Event::Input::Keyboard::S, this, &MotionBlurExample::MoveBackPress);
-	InputManager::AddEvent(Event::Type::OnPress, Event::Input::Keyboard::A, this, &MotionBlurExample::StrafeLeftPress);
-	InputManager::AddEvent(Event::Type::OnPress, Event::Input::Keyboard::D, this, &MotionBlurExample::StrafeRightPress);
-	InputManager::AddEvent(Event::Type::OnRelease, Event::Input::Keyboard::W, this, &MotionBlurExample::MoveFrontRelease);
-	InputManager::AddEvent(Event::Type::OnRelease, Event::Input::Keyboard::S, this, &MotionBlurExample::MoveBackRelease);
-	InputManager::AddEvent(Event::Type::OnRelease, Event::Input::Keyboard::A, this, &MotionBlurExample::StrafeLeftRelease);
-	InputManager::AddEvent(Event::Type::OnRelease, Event::Input::Keyboard::D, this, &MotionBlurExample::StrafeRightRelease);
-	InputManager::AddEvent(Event::Type::OnMove, Event::Input::Mouse::Move, this, &MotionBlurExample::LookTo);
-
-	_strafeLeft = _strafeRight = _moveBack = _moveFront = false;
-	HideMouse();
 }
 
 void MotionBlurExample::Update()
@@ -95,17 +69,19 @@ void MotionBlurExample::Update()
 	// Update Scene
 	Scene->Update(GetTime());
 
+    BaseExample::Update();
+
 	// Game Logic Here
 	CubeObject->SetRotation(Vec3((f32)GetTime()*2, (f32)GetTime()*5, 0));
 	CubeObject->SetPosition(Vec3(sinf(GetTime()*2)*100.f, cos(GetTime()*2)*100.f, CubeObject->GetPosition().z));
 
 	// Render Velocity Map
-	Renderer->PreRender(Camera, Scene);
-	VRenderer->RenderVelocityMap(projection, Camera, Scene);
+	Renderer->PreRender(FPSCamera, Scene);
+	VRenderer->RenderVelocityMap(projection, FPSCamera, Scene);
 
 	// Render Scene
 	EffectManager->CaptureFrame();
-	Renderer->RenderScene(projection, Camera, Scene);
+	Renderer->RenderScene(projection, FPSCamera, Scene);
 	EffectManager->EndCapture();
 
 	((MotionBlurEffect*) MotionBlur)->SetCurrentFPS(fps.getFPS());
@@ -113,29 +89,6 @@ void MotionBlurExample::Update()
 
 	// Render Post Processing
 	EffectManager->ProcessPostEffects(&projection);
-	
-	Vec3 finalPosition;
-	Vec3 direction = Camera->GetDirection();
-	float dt = (float)GetTimeInterval();
-	float speed = dt * 200.f;
-	if (_moveFront)
-	{
-		finalPosition -= direction*speed;
-	}
-	if (_moveBack)
-	{
-		finalPosition += direction*speed;
-	}
-	if (_strafeLeft)
-	{
-		finalPosition += direction.cross(Vec3(0, 1, 0)).normalize()*speed;
-	}
-	if (_strafeRight)
-	{
-		finalPosition -= direction.cross(Vec3(0, 1, 0)).normalize()*speed;
-	}
-
-	Camera->SetPosition(Camera->GetPosition() + finalPosition);
 }
 
 void MotionBlurExample::Shutdown()
@@ -144,7 +97,6 @@ void MotionBlurExample::Shutdown()
 
 	// Remove GameObjects From Scene
 	Scene->Remove(CubeObject);
-	Scene->Remove(Camera);
 
 	CubeObject->Remove(rCube);
 
@@ -152,68 +104,9 @@ void MotionBlurExample::Shutdown()
 	delete cubeMesh;
 	delete rCube;
 	delete CubeObject;
-	delete Camera;
 	delete Renderer;
 	delete VRenderer;
 	delete EffectManager;
-	delete Scene;
 }
 
 MotionBlurExample::~MotionBlurExample() {}
-
-void MotionBlurExample::MoveFrontPress(Event::Input::Info e)
-{
-	_moveFront = true;
-}
-void MotionBlurExample::MoveBackPress(Event::Input::Info e)
-{
-	_moveBack = true;
-}
-void MotionBlurExample::StrafeLeftPress(Event::Input::Info e)
-{
-	_strafeLeft = true;
-}
-void MotionBlurExample::StrafeRightPress(Event::Input::Info e)
-{
-	_strafeRight = true;
-}
-void MotionBlurExample::MoveFrontRelease(Event::Input::Info e)
-{
-	_moveFront = false;
-}
-void MotionBlurExample::MoveBackRelease(Event::Input::Info e)
-{
-	_moveBack = false;
-}
-void MotionBlurExample::StrafeLeftRelease(Event::Input::Info e)
-{
-	_strafeLeft = false;
-}
-void MotionBlurExample::StrafeRightRelease(Event::Input::Info e)
-{
-	_strafeRight = false;
-}
-void MotionBlurExample::LookTo(Event::Input::Info e)
-{
-	if (mouseCenter != GetMousePosition())
-	{
-		mousePosition = InputManager::GetMousePosition();
-		Vec2 mouseDelta = (mousePosition - mouseLastPosition);
-		if (mouseDelta.x != 0 || mouseDelta.y != 0)
-		{
-			counterX -= mouseDelta.x / 10.f;
-			counterY -= mouseDelta.y / 10.f;
-			if (counterY<-80.f) counterY = -80.f;
-			if (counterY>80.f) counterY = 80.f;
-			Quaternion qX, qY;
-			qX.AxisToQuaternion(Vec3(1.f, 0.f, 0.f), DEGTORAD(counterY));
-			qY.AxisToQuaternion(Vec3(0.f, 1.f, 0.f), DEGTORAD(counterX));
-			//                Matrix rotX, rotY;
-			//                rotX.RotationX(DEGTORAD(counterY));
-			//                rotY.RotationY(DEGTORAD(counterX));
-			Camera->SetRotation((qY*qX).GetEulerFromQuaternion());
-			SetMousePosition((int)(mouseCenter.x), (int)(mouseCenter.y));
-			mouseLastPosition = mouseCenter;
-		}
-	}
-}
--- examples/MotionBlurExample/MotionBlurExample.h
+++ examples/MotionBlurExample/MotionBlurExample.h
@@ -6,28 +6,12 @@
 // Description : Rotating Cube Example
 //============================================================================
 
-#ifndef ROTATINGCUBE_H
-#define	ROTATINGCUBE_H
+#ifndef MOTIONBLUREXAMPLE_H
+#define	MOTIONBLUREXAMPLE_H
 
-#if defined(_SDL)
-#include "../WindowManagers/SDL/SDLContext.h"
-#define ClassName SDLContext
-#elif defined(_SDL2)
-#include "../WindowManagers/SDL2/SDL2Context.h"
-#define ClassName SDL2Context
-#else
-#include "../WindowManagers/SFML/SFMLContext.h"
-#define ClassName SFMLContext
-#endif
-
-#include <Pyros3D/Assets/Renderable/Primitives/Shapes/Cube.h>
-#include <Pyros3D/SceneGraph/SceneGraph.h>
+#include "../BaseExample/BaseExample.h"
 #include <Pyros3D/Rendering/Renderer/ForwardRenderer/ForwardRenderer.h>
-#include <Pyros3D/Utils/Colors/Colors.h>
 #include <Pyros3D/Rendering/Components/Rendering/RenderingComponent.h>
-#include <Pyros3D/Rendering/Components/Lights/DirectionalLight/DirectionalLight.h>
-#include <Pyros3D/Rendering/Components/Rendering/RenderingComponent.h>
-#include <Pyros3D/Rendering/Renderer/DebugRenderer/DebugRenderer.h>
 #include <Pyros3D/Rendering/Renderer/SpecialRenderers/VelocityRenderer/VelocityRenderer.h>
 #include <Pyros3D/Rendering/PostEffects/PostEffectsManager.h>
 #include <Pyros3D/Rendering/PostEffects/Effects/MotionBlurEffect.h>
@@ -36,7 +20,7 @@ using namespace p3d;
 #define _STR(path) #path
 #define STR(path) _STR(path)
 
-class MotionBlurExample : public ClassName
+class MotionBlurExample : public BaseExample
 {
 
 public:
@@ -44,15 +28,13 @@ public:
 	MotionBlurExample();
 	virtual ~MotionBlurExample();
 
-	virtual void Init();
-	virtual void Update();
-	virtual void Shutdown();
-	virtual void OnResize(const uint32 width, const uint32 height);
+	void Init();
+	void Update();
+	void Shutdown();
+	void OnResize(const uint32 width, const uint32 height);
 
 private:
 
-	// Scene
-	SceneGraph* Scene;
 	// Renderer
 	ForwardRenderer* Renderer;
 	VelocityRenderer* VRenderer;
@@ -74,25 +56,7 @@ private:
 	GenericShaderMaterial* material;
 	// Texture
 	Texture* texture;
-
-	DebugRenderer* drenderer;
-
-	// Events
-	void MoveFrontPress(Event::Input::Info e);
-	void MoveBackPress(Event::Input::Info e);
-	void StrafeLeftPress(Event::Input::Info e);
-	void StrafeRightPress(Event::Input::Info e);
-	void MoveFrontRelease(Event::Input::Info e);
-	void MoveBackRelease(Event::Input::Info e);
-	void StrafeLeftRelease(Event::Input::Info e);
-	void StrafeRightRelease(Event::Input::Info e);
-	void LookTo(Event::Input::Info e);
-
-	float counterX, counterY;
-	Vec2 mouseCenter, mouseLastPosition, mousePosition;
-	bool _moveFront, _moveBack, _strafeLeft, _strafeRight;
-
 };
 
-#endif	/* ROTATINGCUBE_H */
+#endif	/* MOTIONBLUREXAMPLE_H */
 
